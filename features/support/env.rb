2ace8ac6ca8bdd9639b502001ad05ea3b9cfd219
Gavrishova <anna.gavrishova@gmail.com>
2015-02-15 18:49:47 +0200

    use chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,9 @@
 require 'capybara'
 
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
 Capybara.default_driver = :selenium
 Capybara.default_wait_time = 10
 Capybara.default_selector = :css
