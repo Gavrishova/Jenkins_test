4d60e504aeadbd05a5e8cb26ad73f5cfd4f123b7
Gavrishova <anna.gavrishova@gmail.com>
2015-02-17 21:58:09 +0200

    use ff

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,7 @@
 require 'capybara'
-
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
+#Capybara.register_driver :selenium do |app|
+  #Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
 
 Capybara.default_driver = :selenium
 Capybara.default_wait_time = 10
